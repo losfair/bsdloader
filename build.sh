532045ca5a948eed940ce96900f50d4a0fe9f9bf
losfair <zhy20000919@hotmail.com>
2024-11-02 10:58:19 +0000

    github actions

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 
 cd "$(dirname $0)"
 
-cargo build --release --config "build.rustflags=[\"--remap-path-prefix\", \"$HOME/.cargo=.cargo\", \"--remap-path-prefix\", \"$PWD=project\"]"
+cargo build --release --locked --config "build.rustflags=[\"--remap-path-prefix\", \"$HOME/.cargo=.cargo\", \"--remap-path-prefix\", \"$PWD=project\"]"
